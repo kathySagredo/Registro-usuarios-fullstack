5c2aca9d726b4a5529c6868c9d49153346d1c430
Kathy Sagredo <sagredo.kathy@gmail.com>
2026-07-15 18:09:50 -0400

    chore: configurar proyecto Spring Boot y conexión a Neon

---
+++ registro-usuarios-backend/src/main/java/org/example/registrousuariosbackend/RegistroUsuariosBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.registrousuariosbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroUsuariosBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistroUsuariosBackendApplication.class, args);
+    }
+
+}
